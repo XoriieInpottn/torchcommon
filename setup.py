c74b665011cea0e30bd8924150e2866419c97199
XoriieInpottn <XoriieInpottn@126.com>
2022-09-06 18:26:18 +0800

    New release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
             'torchcommon.nn',
             'torchcommon.optim',
             'torchcommon.utils',
-            'torchcommon.metrics',
+            'torchcommon.utils.metrics',
         ],
         version='0.5.0',
         keywords=['pytorch', 'utilities'],
