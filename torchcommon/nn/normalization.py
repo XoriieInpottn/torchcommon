576438effd6db1f4942ebeb4763a357fbc437804
XoriieInpottn <XoriieInpottn@126.com>
2022-10-08 18:59:31 +0800

    Fix a bug.

--- torchcommon/nn/normalization.py
+++ torchcommon/nn/normalization.py
@@ -13,7 +13,7 @@ __all__ = [
 ]
 
 
-class AdaptiveGroupNorm(nn.Module):
+class AdaptiveGroupNorm(nn.GroupNorm):
 
     def __init__(
             self,
@@ -24,12 +24,22 @@ class AdaptiveGroupNorm(nn.Module):
             device=None,
             dtype=None
     ) -> None:
-        super(AdaptiveGroupNorm, self).__init__()
         small_group_size = num_channels // num_groups
         big_group_size = small_group_size + 1
-
         self.num_groups_big = num_channels % num_groups
         self.num_channels_big = big_group_size * self.num_groups_big
+        self.num_groups = num_groups - self.num_groups_big
+        self.num_channels = num_channels - self.num_channels_big
+
+        super(AdaptiveGroupNorm, self).__init__(
+            num_groups=self.num_groups,
+            num_channels=self.num_channels,
+            eps=eps,
+            affine=affine,
+            device=device,
+            dtype=dtype
+        )
+
         self.norm_big = nn.GroupNorm(
             self.num_groups_big,
             self.num_channels_big,
@@ -39,24 +49,13 @@ class AdaptiveGroupNorm(nn.Module):
             dtype=dtype
         ) if self.num_groups_big != 0 else None
 
-        self.num_groups = num_groups - self.num_groups_big
-        self.num_channels = num_channels - self.num_channels_big
-        self.norm = nn.GroupNorm(
-            self.num_groups,
-            self.num_channels,
-            eps=eps,
-            affine=affine,
-            device=device,
-            dtype=dtype
-        )
-
     def forward(self, x: torch.Tensor):
         if self.norm_big is None:
-            return self.norm(x)
+            return super(AdaptiveGroupNorm, self).forward(x)
         else:
             x1 = x[:, :self.num_channels, ...]
             x2 = x[:, self.num_channels:, ...]
             return torch.cat([
-                self.norm(x1),
+                super(AdaptiveGroupNorm, self).forward(x1),
                 self.norm_big(x2)
             ], 1)
