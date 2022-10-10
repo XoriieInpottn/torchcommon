49c4063807ea66ba266f9c2aa6cd6aa8ece1482e
XoriieInpottn <XoriieInpottn@126.com>
2022-10-10 11:43:24 +0800

    New features.

--- torchcommon/config.py
+++ torchcommon/config.py
@@ -9,9 +9,13 @@ __all__ = [
 
 class BaseConfig(object):
 
+    def __init__(self, args=None):
+        if args is not None:
+            self.load(args)
+
     def load(self, args):
-        for name, value in self.__dict__.items():
-            if name.startswith('_') or callable(value):
+        for name, value in self._get_attributes().items():
+            if name.startswith('_'):
                 continue
             if hasattr(args, name):
                 setattr(self, name, getattr(args, name))
@@ -19,19 +23,26 @@ class BaseConfig(object):
     def __str__(self):
         pairs = []
         max_len = 0
-        for name, value in self.__dict__.items():
-            if name.startswith('_') or callable(value):
+        for name, value in self._get_attributes().items():
+            if name.startswith('_'):
                 continue
+
             if value is None:
                 value = 'None'
             elif isinstance(value, str):
                 value = f'\'{value}\''
             else:
                 value = str(value)
-            l = len(name)
-            if l > max_len:
-                max_len = l
+
+            break_line = value.find('\n')
+            if break_line >= 0:
+                value = value[:break_line]
+
             pairs.append((name, value))
+
+            name_len = len(name)
+            if name_len > max_len:
+                max_len = name_len
 
         buffer = io.StringIO()
         for name, value in pairs:
@@ -40,3 +51,11 @@ class BaseConfig(object):
             buffer.write(value)
             buffer.write('\n')
         return buffer.getvalue()
+
+    def _get_attributes(self):
+        d = {}
+        for name, value in self.__class__.__dict__.items():
+            d[name] = value
+        for name, value in self.__dict__.items():
+            d[name] = value
+        return d
